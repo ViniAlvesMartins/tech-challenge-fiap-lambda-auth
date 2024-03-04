78ff754e70201c524f30eb8f5c7062136bc7fd66
Icaro90 <icaro.nove-nta@hotmail.com>
2024-03-03 23:28:45 -0300

    ajuste config lambda

--- main.tf
+++ main.tf
@@ -2,12 +2,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "archive_file" "main" {
+  type        = "zip"
+  source_dir  = "app/src"
+  output_path = "${path.module}/archive_files/function.zip"
+
+  depends_on = [null_resource.main]
+}
+
+resource "null_resource" "main" {
+
+  triggers = {
+    updated_at = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+    yarn
+    EOF
+
+    working_dir = "${path.module}/lambda/function"
+  }
+}
+
 resource "aws_lambda_function" "lambda" {
   function_name    = "lbd-authorizer"
-  role             = aws_iam_role.temp_lambda.arn
+  role             = aws_iam_role.lambda.arn
   handler          = "index.handler"
-  runtime          = "nodejs18.x"
-  filename         = "lambda.zip"
+  runtime          = var.lambda_runtime
+  filename         = "${path.module}/.terraform/archive_files/function.zip"
+  timeout          = var.lambda_timeout
+  source_code_hash = data.archive_file.main.output_base64sha256
 }
 
 resource "aws_iam_role" "lambda" {
@@ -30,4 +55,12 @@ resource "aws_iam_role" "lambda" {
 resource "aws_iam_role_policy_attachment" "lambda" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.lambda.name
+}
+
+resource "aws_lambda_permission" "lambda" {
+  statement_id  = "AllowExecutionFromApiGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:us-east-1:590183718917:2k35c7thu4/*"
 }
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "lambda_subnet_ids" {
+  description = "List of subnets in which the action runners will be launched, the subnets needs to be subnets in the `vpc_id`."
+  type        = list(string)
+  default     = []
+}
+
+variable "lambda_security_group_ids" {
+  description = "List of security group IDs associated with the Lambda function."
+  type        = list(string)
+  default     = []
+}
+
+variable "lambda_zip" {
+  description = "File location of the lambda zip file."
+  type        = string
+  default     = null
+}
+
+variable "lambda_memory_size" {
+  description = "Memory size limit in MB for lambda."
+  type        = number
+  default     = 256
+}
+
+variable "lambda_timeout" {
+  description = "Time out of the lambda in seconds."
+  type        = number
+  default     = 10
+}
+
+variable "lambda_runtime" {
+  description = "AWS Lambda runtime."
+  type        = string
+  default     = "nodejs18.x"
+}
